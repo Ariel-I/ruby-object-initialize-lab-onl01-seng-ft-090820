cc5d90164821190536dbaa19dd26fd21793b17ef
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-17 05:23:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,12 +7,14 @@ class Dog
   
   def name=(dog_name)
     @name = dog_name 
-    @breed = dog_breed 
   end 
   
   def name 
     @name 
   end
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
   
   def breed 
     @breed 
