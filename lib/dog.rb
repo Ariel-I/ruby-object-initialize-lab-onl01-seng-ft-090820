dfb558c035fee951c1e0dd89b61f9293a0984c8d
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-17 04:26:02 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,31 @@
+class Dog 
+  
+  def initialize(name)
+    @name = name
+  end 
+  
+  def dog=(name)
+    @name = name 
+  end 
+  
+  def name  
+    @name 
+  end
+  
+  
+  def initialize(breed)
+    @breed = breed
+  end 
+  
+  def breed=(breed)
+    @breed = breed 
+  end 
+  
+def breed
+  @breed  
+  end
+  
+end
+  
+  
+  
---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+  
+  def initialize(name)
+    @name = name
+  end 
+  
+  def person=(name)
+    @name = name 
+  end 
+  
+  def person 
+    @name 
+  end
+  
+end
